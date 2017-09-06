658705d81c88e7837e644bafcd3fd0912d11388c
greg@foodrev.org <greg@foodrev.org>
2017-09-05 17:33:34 -0700

    add basic linechart example

--- app/src/main/java/com/dastechlabs/sensordashboard/MainActivity.java
+++ app/src/main/java/com/dastechlabs/sensordashboard/MainActivity.java
@@ -1,13 +1,44 @@
 package com.dastechlabs.sensordashboard;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private LineChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        chart = (LineChart) findViewById(R.id.line_chart);
+
+        List<Entry> entries = new ArrayList<Entry>();
+
+
+        for (int i = 0; i < 10; i++) {
+            entries.add(new Entry(i,i));
+        }
+
+        LineDataSet dataSet = new LineDataSet(entries, "Label");
+        dataSet.setColor(Color.GREEN);
+        dataSet.setValueTextColor(Color.RED);
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate(); // refresh
+
+
     }
 }
